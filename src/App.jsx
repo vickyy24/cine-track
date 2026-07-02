ab31d6696e0b1b1b2fb6e8147ae9fc8f81116897
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-07-03 01:08:57 +0530

    Install and configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			<h1 className="text-4xl font-bold text-blue-600">
+				CineTrack
+			</h1>
+		</div>
+	);
+}
+
+export default App;
